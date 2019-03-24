b4f8f88446c2e15ca447dcc1f37528952989c808
calvin-fisher <1797159+calvin-fisher@users.noreply.github.com>
2019-03-24 12:41:15 -0500

    Initial commit

---
+++ build.bat
@@ -0,0 +1,6 @@
+if EXIST "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SimpleFurniture" (
+    RMDIR /S /Q "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SimpleFurniture\*" )
+) ELSE (
+    MKDIR "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SimpleFurniture\
+)
+xcopy /S SimpleFurniture "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SimpleFurniture"
